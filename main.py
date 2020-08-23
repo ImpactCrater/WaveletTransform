270f3556b3b8b09c8a419f420a0428f9d1f482a1
ImpactCrater <hq45896@wa2.so-net.ne.jp>
2020-08-23 15:18:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,28 +6,35 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 import plotter
-import wavelet_transform as WT
+import waveletTransform as WT
 
-def ImageProcessing(imagePath):
+def ImageProcessing(imagePath, runForward = True, runInverse = False, n = 3):
   print("Now loading the image...")
   print("Image Path = {0}".format(imagePath))
   img = cv2.imread(imagePath)
-
   originalSize = img.shape[:2]
-  resultImage = WT.WaveletTransform(img, n = 4)
-  resultImage = WT.InverseWaveletTransform(resultImage, n = 4)
-  resultImage = resultImage[:originalSize[0], :originalSize[1], :]
 
-  resultImage = resultImage.clip(0,255)
-  resultImage = np.uint8(resultImage)
+  if runForward:
+    img = WT.WaveletTransform(img, n)
+    resultImage = img.copy()
+    resultImage = np.uint8(resultImage)
+    plt = plotter.Plotter(plot = False, num_images=1, out_filename="testResultForward.png")
+    plt.save(resultImage)
+  else:
+    print(runForward)
 
-  plt = plotter.Plotter(plot = False, num_images=1, out_filename="testResult.png")
-  plt.save(resultImage)
+  if runInverse:
+    resultImage = WT.InverseWaveletTransform(img, n)
+    resultImage = resultImage[:originalSize[0], :originalSize[1], :]
+    resultImage = resultImage.clip(0,255)
+    resultImage = np.uint8(resultImage)
+    plt = plotter.Plotter(plot = False, num_images=1, out_filename="testResultInverse.png")
+    plt.save(resultImage)
 
 
 def main():
   try:
-    ImageProcessing(sys.argv[1])
+    ImageProcessing(sys.argv[1], sys.argv[2], sys.argv[3])
   except Exception as e:
     print(e)
 
